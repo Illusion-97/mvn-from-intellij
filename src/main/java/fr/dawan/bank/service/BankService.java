241e8d6de351a1423e940c43aeaaa861b99d73be
Yanis Adekalom <yanis971.60@gmail.com>
2023-10-31 15:31:50 +0100

    Feat : DWT

--- src/main/java/fr/dawan/bank/service/BankService.java
+++ src/main/java/fr/dawan/bank/service/BankService.java
@@ -2,6 +2,7 @@ package fr.dawan.bank.service;
 
 import fr.dawan.bank.entities.Account;
 import fr.dawan.bank.exceptions.AccountNotFoundException;
+import fr.dawan.bank.exceptions.InsufficientFundsException;
 import fr.dawan.bank.mappers.AccountMapper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -26,9 +27,19 @@ public class BankService {
         return accounts.get(name);
     }
 
-    private static String getAccountData(String name) throws AccountNotFoundException {
+    public static String getAccountData(String name) throws AccountNotFoundException {
         //Objects.toString prévient les erreurs en cas de valeur nulle à mettre au format String en donnant la possibilité de définir une valeur par défaut
         // Garder le réflexe de remplacer 'value.toString()' par Objects.toString(value,"default")
         return Objects.toString(accountMapper.toDto(getAccount(name)), "Erreur durant la récupération du compte");
+    }
+
+    public static void deposit(String name, double amount) throws AccountNotFoundException {
+        getAccount(name).deposit(amount);
+    }
+    public static void withdraw(String name, double amount) throws AccountNotFoundException, InsufficientFundsException {
+        getAccount(name).withdraw(amount);
+    }
+    public static void transfer(String name, String targetName, double amount) throws AccountNotFoundException, InsufficientFundsException {
+        getAccount(name).transfer(amount,getAccount(targetName));
     }
 }
