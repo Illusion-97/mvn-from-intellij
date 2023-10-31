9fe1f64ba67940f4ed747aed90a1a196933a3715
Yanis Adekalom <yanis971.60@gmail.com>
2023-10-31 09:49:40 +0100

    Feat : SpeakListener

--- src/main/java/fr/dawan/Main.java
+++ src/main/java/fr/dawan/Main.java
@@ -1,10 +1,26 @@
 package fr.dawan;
 
-import fr.dawan.bank.entities.Account;
+
+import fr.dawan.discord.SpeakListener;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class Main {
     public static void main(String[] args) {
-        Account account = new Account();
-        System.out.println("Hello world!");
+        JDABuilder.createDefault(args[0])
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .enableIntents(GatewayIntent.GUILD_MEMBERS,
+                        GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.MESSAGE_CONTENT
+                )
+                .setChunkingFilter(ChunkingFilter.ALL)
+                .setBulkDeleteSplittingEnabled(false)
+                .setActivity(Activity.watching("le coffre de la banque"))
+                .addEventListeners(new SpeakListener())
+                .build();
     }
 }
--- src/main/java/fr/dawan/discord/SpeakListener.java
+++ src/main/java/fr/dawan/discord/SpeakListener.java
@@ -1,6 +1,22 @@
 package fr.dawan.discord;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 public class SpeakListener extends ListenerAdapter {
+    @Override
+    public void onGuildReady(GuildReadyEvent event) {
+        Guild serveur = event.getGuild();
+        SlashCommandData speakCommand = Commands.slash("speak", "Faire Parler son bot");
+        serveur.updateCommands().addCommands(speakCommand).queue();
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        super.onSlashCommandInteraction(event);
+    }
 }
