24d34da3682a460ef8e24e558574870198c0f581
Yanis Adekalom <yanis971.60@gmail.com>
2023-10-31 10:37:52 +0100

    Fix : SpeakListener

--- src/main/java/fr/dawan/discord/SpeakListener.java
+++ src/main/java/fr/dawan/discord/SpeakListener.java
@@ -4,19 +4,34 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+
+import java.util.Objects;
 
 public class SpeakListener extends ListenerAdapter {
     @Override
     public void onGuildReady(GuildReadyEvent event) {
         Guild serveur = event.getGuild();
         SlashCommandData speakCommand = Commands.slash("speak", "Faire Parler son bot");
+        OptionData text = new OptionData(OptionType.STRING, "text", "Le texte à afficher", true);
+        speakCommand.addOptions(text);
         serveur.updateCommands().addCommands(speakCommand).queue();
     }
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        super.onSlashCommandInteraction(event);
+            String commandName = event.getName();
+            if(commandName.equals("speak")) {
+                // Le retour de getOption peur être null, c'est pour cela qu'on a un avertissement
+                // event.getOption("text").getAsString();
+                /*OptionMapping option = event.getOption("text");
+                String optionAsString = option != null ? option.getAsString() : "Hello Humans !";
+                event.reply(optionAsString).queue();*/
+                event.reply(Objects.requireNonNull(event.getOption("text")).getAsString()).queue();
+            }
     }
 }
